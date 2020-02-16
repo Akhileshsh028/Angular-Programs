e8c148993abed6e4b51b05dc60aee5cda8edae1e
Akhileshsh028 <Akhileshsh028@outlook.com>
2020-02-16 09:03:04 +0530

    addition option is heaving concatination error

---
+++ 3_Testing_Filters/Scripts/Service_Factory_Js.js
@@ -0,0 +1,26 @@
+﻿var app = angular.module("Service_FactoryApp", []);
+
+app.controller("Service_Controller", function ($scope) {
+
+    $scope.arithops = function (num) {
+        switch (num) {
+            case 1: var result = $scope.firstnumber + $scope.secondnumber;
+                $scope.resultMessage = "The addition Result is : " + result;
+                break;
+
+            case 2: var result = $scope.firstnumber - $scope.secondnumber;
+                $scope.resultMessage = "The Substraction Result is : " + result;
+                break;
+
+            case 3: var result = $scope.firstnumber * $scope.secondnumber;
+                $scope.resultMessage = "The Multiplication Result is : " + result;
+                break;
+
+            case 4: var result = $scope.firstnumber / $scope.secondnumber;
+                $scope.resultMessage = "The Division Result is : " + result;
+                break;
+
+            default: $scope.resultMessage = "Wrong Choise guys working on further extension.";
+        }           
+    };
+});
