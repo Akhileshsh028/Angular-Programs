1591f6d923ec7f03f664817e2e2dcfc731539616
Akhileshsh028 <Akhileshsh028@outlook.com>
2020-02-16 13:42:27 +0530

    in service we need to convert the partermters in integer

--- 3_Testing_Filters/Scripts/Service_Factory_Js.js
+++ 3_Testing_Filters/Scripts/Service_Factory_Js.js
@@ -1,26 +1,46 @@
 ﻿var app = angular.module("Service_FactoryApp", []);
 
-app.controller("Service_Controller", function ($scope) {
+app.service("ArithOpsService", function () {
+
+    this.add = function (a, b) {
+        return a + b;
+    };
+
+    this.sub = function (a, b) {
+        return a - b;
+    };
+
+    this.mul = function (a, b) {
+        return a * b;
+    };
+
+    this.div = function (a, b) {
+        return a / b;
+    };
+     
+});
+
+app.controller("Service_Controller", function ($scope, ArithOpsService) {
 
     $scope.arithops = function (num) {
         switch (num) {
-            case 1: var result = $scope.firstnumber + $scope.secondnumber;
-                $scope.resultMessage = "The addition Result is : " + result;
-                break;
+            case 1: var result = ArithOpsService.add($scope.firstnumber , $scope.secondnumber);
+                    $scope.resultMessage = "The addition Result is : " + result;
+                    break;
 
-            case 2: var result = $scope.firstnumber - $scope.secondnumber;
+            case 2: var result = ArithOpsService.sub($scope.firstnumber , $scope.secondnumber);
                 $scope.resultMessage = "The Substraction Result is : " + result;
                 break;
 
-            case 3: var result = $scope.firstnumber * $scope.secondnumber;
+            case 3: var result = ArithOpsService.mul($scope.firstnumber , $scope.secondnumber);
                 $scope.resultMessage = "The Multiplication Result is : " + result;
                 break;
 
-            case 4: var result = $scope.firstnumber / $scope.secondnumber;
+            case 4: var result = ArithOpsService.div($scope.firstnumber , $scope.secondnumber);
                 $scope.resultMessage = "The Division Result is : " + result;
                 break;
 
-            default: $scope.resultMessage = "Wrong Choise guys working on further extension.";
+            default: $scope.resultMessage = "Wrong Choose guys working on further extension.";
         }           
     };
 });
