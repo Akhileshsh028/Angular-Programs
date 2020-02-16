251e1132ec39373f8c629bf3537ee39fec820c27
Akhileshsh028 <Akhileshsh028@outlook.com>
2020-02-16 13:55:00 +0530

    using factory

--- 3_Testing_Filters/Scripts/Service_Factory_Js.js
+++ 3_Testing_Filters/Scripts/Service_Factory_Js.js
@@ -20,23 +20,47 @@ app.service("ArithOpsService", function () {
      
 });
 
-app.controller("Service_Controller", function ($scope, ArithOpsService) {
+app.factory("ArithOpsFactory", function () {
+
+    var obj = {};
+
+    obj.add = function (a, b) {
+        return parseInt(a) + parseInt(b);
+    };
+
+    obj.sub = function (a, b) {
+        return a - b;
+    };
+
+    obj.mul = function (a, b) {
+        return a * b;
+    };
+
+    obj.div = function (a, b) {
+        return a / b;
+    };
+
+    return obj;
+
+});
+
+app.controller("Service_Controller", function ($scope, ArithOpsFactory) {
 
     $scope.arithops = function (num) {
         switch (num) {
-            case 1: var result = ArithOpsService.add($scope.firstnumber , $scope.secondnumber);
+            case 1: var result = ArithOpsFactory.add($scope.firstnumber , $scope.secondnumber);
                     $scope.resultMessage = "The addition Result is : " + result;
                     break;
 
-            case 2: var result = ArithOpsService.sub($scope.firstnumber , $scope.secondnumber);
+            case 2: var result = ArithOpsFactory.sub($scope.firstnumber , $scope.secondnumber);
                 $scope.resultMessage = "The Substraction Result is : " + result;
                 break;
 
-            case 3: var result = ArithOpsService.mul($scope.firstnumber , $scope.secondnumber);
+            case 3: var result = ArithOpsFactory.mul($scope.firstnumber , $scope.secondnumber);
                 $scope.resultMessage = "The Multiplication Result is : " + result;
                 break;
 
-            case 4: var result = ArithOpsService.div($scope.firstnumber , $scope.secondnumber);
+            case 4: var result = ArithOpsFactory.div($scope.firstnumber , $scope.secondnumber);
                 $scope.resultMessage = "The Division Result is : " + result;
                 break;
 
